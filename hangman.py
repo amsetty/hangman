fecc578945dc087fe3b23eddf03f866db43eae2d
Aravind Madhamsetty <aravind@Aravinds-MacBook-Pro.local>
2022-07-21 00:26:50 -0700

    checkpoint1

--- hangman.py
+++ hangman.py
@@ -4,16 +4,40 @@ import string
 
 def get_valid_word(words):
     word = random.choice(words)
-    while '-' or ' ' in word:
+    while '-' in word or ' ' in word:
         word = random.choice(words)
-
-    return word 
+    
+    return word.upper() 
 
 def hangman():
     word = get_valid_word(words)
     word_letters = set(word) # letters in the word
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
+
+
+    #getting user input
+    while len(word_letters) > 0:
+        print('you have used these letters:',' '.join(used_letters))
+
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+
+        print('Current word:',' '.join(word_list))
+
+
+        user_letter = input('Guess a letter:').upper()
+        
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+        
+        elif user_letter in used_letters:
+            print('You have used this letter. Please try again.')
+        
+        else:
+            print('Invalid character. Please try again.')
     
-user_input = input('type something:')
-print(user_input)
+    print('You have guessed it right the word is:',word)
+
+hangman()
